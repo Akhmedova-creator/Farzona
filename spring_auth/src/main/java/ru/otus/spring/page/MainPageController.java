2e7f8a77b8dd7a84258a07650452c0c7d88d8a76
sdddd <fazik_agent007@mail.ru>
2023-07-03 20:40:42 +0500

    ааа et

--- spring_auth/src/main/java/ru/otus/spring/page/MainPageController.java
+++ spring_auth/src/main/java/ru/otus/spring/page/MainPageController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainPageController {
-    @GetMapping("/list")
+    @GetMapping("/")
     public String mainPage() {
         return "main";
     }
