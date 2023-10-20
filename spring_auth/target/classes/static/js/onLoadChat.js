186cb6b72ff30da834f02b1e1b147e030a1fbb90
sdddd <fazik_agent007@mail.ru>
2023-10-21 01:34:04 +0500

    ааа et

--- spring_auth/target/classes/static/js/onLoadChat.js
+++
@@ -1,9 +0,0 @@
-$(window).on('load', function() {
-
-  //  заполнение логтн и пароль
-    $('#myLogin').val($.cookie('user'))
-    $('#myPassword').val($.cookie('pass'))
-    $.get("/getOneUser", {login: $('#myLogin').val(), pass: $('#myPassword').val()}).done(function (user) {
-        $('#myId').val(user.id) ;
-    })
-});
