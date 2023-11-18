c5e65715eaec93731b0470d1365a5d7419b42577
Akhmedova-creator <55805446+Akhmedova-creator@users.noreply.github.com>
2023-11-19 00:46:56 +0500

    Update MainPageController.java

--- spring_auth/src/main/java/ru/otus/spring/page/MainPageController.java
+++ spring_auth/src/main/java/ru/otus/spring/page/MainPageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainPageController {
     @GetMapping("/")
     public String mainPage() {
-        return "main";
+        return "index";
     }
 
     @GetMapping("/chat")
